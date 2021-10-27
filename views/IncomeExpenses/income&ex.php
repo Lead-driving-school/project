77ed44f0e8e4579a196f3f12e844a62df6048808
Gihan <gihanweerasinghe21@gmail.com>
2021-10-27 17:21:16 +0530

    Income and expences page eka costamize kara

---
+++ views/IncomeExpenses/income&ex.php
@@ -0,0 +1,38 @@
+    <link rel="stylesheet" href="<?php echo URL?>public/css/Manager/main/header.css">
+    <link rel="stylesheet" href="<?php echo URL?>public/css/Manager/main/structure.css">
+    <link rel="stylesheet" href="<?php echo URL?>public/css/IncomeExpenses/income&Expenses/structure.css">
+    <link rel="stylesheet" href="<?php echo URL?>public/css/Manager/main/Horizontaltab.css">
+
+<div class="container">
+            <!--Header-->
+            <h1>Income & Expenses</h1>
+            <div class="Horizontal-tab">
+                <div class="tabContainer">
+                  <button class="page-change-button" onclick="changeFilter(event,'All')">Day</button>
+                  <button class="page-change-button" onclick="changeFilter(event,'Ssessions')">Month</button>
+                  <button class="page-change-button" onclick="changeFilter(event,'Exams')">Year</button>
+                </div>
+                <div class="date-container">
+                    <Label for="date-container"class="date-label">Date</Label><input type="date" name="date" id="date">
+                </div>
+            </div>
+            <div class="body-container">
+                <div class="income-expense-box"> 
+                    <div class="income-row">
+                        <div class="name-field">Income</div>
+                        <div class="value-field">15,000LKR</div>
+                        <div class="view-more"><button id="viewIncome" name="viewIncome">View more</button></div>
+                    </div>
+                    <div class="expense-row">
+                        <div class="name-field">Expenses</div>
+                        <div class="value-field">10,000LKR</div>
+                        <div class="view-more"><button id="viewExpenses" name="viewExpenses">View more</button></div>
+                    </div>
+                    <div class="profit-row">
+                        <div class="name-field">Profit</div>
+                        <div class="value-field">5,000LKR</div>
+                        <div class="view-more"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
