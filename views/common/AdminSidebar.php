d9b9b9d9413c116849808d69ea14ee187b746975
Gihan <gihanweerasinghe21@gmail.com>
2021-10-27 18:40:44 +0530

    admin sidebar ekata icons damma

--- views/common/AdminSidebar.php
+++ views/common/AdminSidebar.php
@@ -12,12 +12,12 @@
             <span class="link-name">Staff</span>
         </a>
         <a class="<?php echo (getLink()=="http://localhost/project/Admin/packages")?"list-item-active":"list-item";?>" href="<?php echo URL?>Admin/packages">
-            <span class="link-icon">&#xf187</span>
+            <span class="link-icon">&#xf0d6</span>
             <span class="link-name">Packages & prices</span>
         </a>
         
         <a class="<?php echo (getLink()=="http://localhost/project/Admin/employeeAccounts")?"list-item-active":"list-item";?>" href="<?php echo URL?>Admin/employeeAccounts">
-            <span class="link-icon">&#xf02d</span>
+            <span class="link-icon">&#xf115</span>
             <span class="link-name">Accounts</span>
         </a>
         <a class="<?php echo (getLink()=="http://localhost/project/Admin/vehicles")?"list-item-active":"list-item";?>" href="<?php echo URL?>Admin/vehicles">
@@ -31,13 +31,18 @@
         </a>
 
         <a class="<?php echo (getLink()=="http://localhost/project/Admin/viewComlain")?"list-item-active":"list-item";?>" href="<?php echo URL?>Admin/viewComlain">
-            <span class="link-icon">&#xf080</span>
+            <span class="link-icon">&#xf0e6</span>
             <span class="link-name">Complains & Reviews</span>
         </a>
 
-        <a class="<?php echo (getLink()=="http://localhost/project/Admin/a")?"list-item-active":"list-item";?>" href="<?php echo URL?>IncomeExpenses">
-            <span class="link-icon">&#xf011</span>
+        <a class="<?php echo (getLink()=="http://localhost/project/Admin/IncomeExpenses")?"list-item-active":"list-item";?>" href="<?php echo URL?>Admin/IncomeExpenses">
+            <span class="link-icon">&#xf09d</span>
             <span class="link-name">Income & Expences</span>
+        </a>
+
+        <a class="<?php echo (getLink()=="http://localhost/project/Admin/a")?"list-item-active":"list-item";?>" href="<?php echo URL?>Admin/IncomeExpenses">
+            <span class="link-icon">&#xf08b</span>
+            <span class="link-name">Logout</span>
         </a>
     </div>
 </div>
