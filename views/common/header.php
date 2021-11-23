20c89cb99e74fd516df9f41ad473ed4a7a6c8971
Gihan <gihanweerasinghe21@gmail.com>
2021-11-23 15:47:28 +0530

    responsive continuening

--- views/common/header.php
+++ views/common/header.php
@@ -1,6 +1,9 @@
 <link rel="stylesheet" href="<?php echo URL?>public/css/common/header.css">
 <div class="abc-header">
         <div class="header-container">
+            <div class="sidebar-button">
+                
+            </div>
             <div class="header-logo">
             <img src="<?php echo URL?>public/images/logo.png" alt="">
             </div>
