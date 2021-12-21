2ea3bf877362f72596db05dd3eded74551e00b58
Zen_99 <2019cs161@stu.ucsc.lk>
2021-12-21 10:16:26 +0530

    students' expenses

--- models/IncomeExpenses_Model.php
+++ models/IncomeExpenses_Model.php
@@ -10,4 +10,14 @@ class IncomeExpenses_Model extends Model{
         $result=$this->db->runQuery("SELECT other_expenses.oexpense_id,other_expenses.title,other_expenses.description,other_expenses.recorded_date_time,other_expenses.amount,employee.name from ((other_expenses INNER JOIN manager on manager.employee_id=other_expenses.employee_id) INNER JOIN employee on employee.employee_id=manager.employee_id)");
         return $result;
     }
+
+    function getStudentExpenses(){
+        $result=$this->db->runQuery("SELECT initial_service_expenses_submits.student_id,student.init_name,initial_service_expenses_submits.date_time,initial_service_expenses_submits.status,initial_service_expenses.amount from((student INNER JOIN initial_service_expenses_submits on initial_service_expenses_submits.student_id=student.student_id) INNER JOIN initial_service_expenses on initial_service_expenses.ischarge_id=initial_service_expenses_submits.ischarge_id) order by initial_service_expenses_submits.student_id");
+        return $result;
+    }
+    function acceptExpense($studentId){
+        $studentId=intval($studentId);
+        $result=$this->db->runQuery("UPDATE initial_service_expenses_submits SET status='accepted' WHERE student_id=$studentId");
+        return $studentId;
+    }
 }
