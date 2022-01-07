f0023832752299182399b2d46535d12480b82eb7
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2022-01-07 20:03:15 +0530

    going not going eka replace

--- public/js/student/TodaySession.js
+++ public/js/student/TodaySession.js
@@ -4,16 +4,19 @@ function viewTodaySession(){
     let httprequest=new XMLHttpRequest();
     httprequest.onreadystatechange=function(){
         if (httprequest.readyState===4 && httprequest.status===200){
-            console.log(httprequest.responseText)
+            // console.log(httprequest.responseText)
            const obj=JSON.parse(httprequest.responseText)
            console.log(obj)
-           for(var i=0; i<obj.length;i++){
+           for(var i=0; i<obj.length;i++){sessionid
+            var sessionid=obj[i].session_id;
             row.innerHTML+=
             '<div class="row"><div class="col">'+obj[i].session_id +'</div>'+
             '<div class="col">'+obj[i].session_title+'</div>'+
             '<div class="col">'+obj[i].session_date+'</div>'+
             '<div class="col">'+obj[i].session_time+'</div>'+
-            '<div class="col" id="going-col"> <button class="going" onclick="going()">Going</button></div><div class="col" > <button class="notgoing-click-going">Not Going</button></div></div>'
+            '<div class="col" id="going-col"> <button class="going" id="'+sessionid+'" onclick="going('+sessionid+')">Going</button></div>'+
+            '<div class="col"><button class="notgoing" id="'+sessionid+'" onclick="notgoing('+sessionid+')">Not Going</button></div></div>'
+            
 
         }
 
@@ -26,11 +29,15 @@ function viewTodaySession(){
     httprequest.open("POST",url,true)
     httprequest.send()
 }
-
-function going(){
-    document.getElementById("notgoing-col").classList.replace("notgoing-click-going","notgoing");
-    console.log("hi");
-
+function going(objId){
+    console.log(objId);
+    document.getElementById(objId).classList.replace("notgoing","notgoing-click-going");
     
+      
 }
+
+function notgoing(objId){
+    document.getElementById(objId).classList.replace("going","going-click-notgoing"); 
+}
+
 viewTodaySession()
