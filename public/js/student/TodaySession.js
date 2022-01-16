dd02d47917e613cfff77087c49162246ec45d622
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2022-01-16 17:00:15 +0530

    today session 2

--- public/js/student/TodaySession.js
+++ public/js/student/TodaySession.js
@@ -47,15 +47,18 @@ function going(objId){
     // console.log(objId);
     // document.getElementById(objId).classList.replace("notgoing","notgoing-click-going");
     const row = document.getElementById("data");
+    var status="going";
     let httprequest=new XMLHttpRequest();
     httprequest.onreadystatechange=function(){
         if(httprequest.readyState===4 && httprequest.status===200){
+            console.log("Hi");
+            window.location.href="http://localhost/project/Student/markGoingNotGoingLogic";
 
         }
     }
 
-    var url="http://localhost/project/Student/markGoingNotGoingLogic";
-    httprequest.open("POST",url,true)
+    var url_going="http://localhost/project/Student/markGoingNotGoingLogic_Going/"+status;
+    httprequest.open("POST",url_going,true)
     httprequest.send()
 
       
